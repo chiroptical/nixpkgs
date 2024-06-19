602d573fbeb763f30e1bf898ddb055648a4989cf
chiroptical <chiroptical@gmail.com>
2024-06-19 14:53:13 -0400

    Try most recent commit

--- pkgs/development/interpreters/erlang/27.nix
+++ pkgs/development/interpreters/erlang/27.nix
@@ -1,6 +1,7 @@
 { mkDerivation }:
 
 mkDerivation {
+  rev = "0d48eb93a0a1e71e7fda57f993eef0318f919f71";
   version = "27.0";
-  sha256 = "sha256-YZWBLcpkm8B4sjoQO7I9ywXcmxXL+Dvq/JYsLsr7TO0=";
+  sha256 = "sha256-IpGwOXzOcOy3jIS7eQr6VbWzZqKTMu+qtoorZDIJb/4=";
 }
